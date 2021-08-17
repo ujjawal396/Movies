5e05382e167590c3743f4ddff73e62d5f430ebe0
ujjawaljha <ujjawaljha08@gmail.com>
2021-08-17 19:25:03 +0530

    restarted the whole lecture

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,40 +1,21 @@
 import React from "react";
-import Filter from "./Filter";
-import Navbar from "./Navbar";
 
 class App extends React.Component {
-  state = {
-    movies: [],
-    genre: [],
-  };
-
   componentDidMount() {
-    let f = async () => {
-      let responseGenre = await fetch("/genre");
-      let responseMovies = await fetch("/movies");
-      let moviesJson = await responseMovies.json();
-      let genreJson = await responseGenre.json();
+    //i will get data here
 
-      this.setState({
-        movies: moviesJson,
-        genre: genreJson,
-      });
+    let f = async () => {
+      let result = await fetch("/movies");//browser function
+      console.log(result);
+      let json = await result.json();
+      console.log(json);
     };
 
     f();
   }
 
   render() {
-    return (
-      <div>
-        <Navbar />
-        
-
-        <div className="row">
-          <Filter genreData = {this.state.genre}/>
-        </div>
-      </div>
-    );
+    return <div></div>;
   }
 }
 
