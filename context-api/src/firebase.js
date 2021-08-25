d9162deed8e3c40875dcb18a58915c57f0d24f95
ujjawaljha <ujjawaljha08@gmail.com>
2021-08-25 16:36:31 +0530

    firebase introduced

---
+++ context-api/src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAZZvCZz3hCg1-tZYvlwPptTv0y7xmgRMc",
+    authDomain: "context-27b98.firebaseapp.com",
+    projectId: "context-27b98",
+    storageBucket: "context-27b98.appspot.com",
+    messagingSenderId: "263761659238",
+    appId: "1:263761659238:web:8dea60fe2c76dae0a7f4a7"
+  };
+
+
+  firebase.initializeApp(firebaseConfig);
